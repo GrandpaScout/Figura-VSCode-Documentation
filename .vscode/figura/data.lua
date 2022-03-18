4c81b23213bfb1fe966ef0ab18cb48fd87d70bef
KitCat962 <kitcatt962@gmail.com>
2022-03-18 16:44:08 -0400

    Create data.lua

---
+++ .vscode/figura/data.lua
@@ -0,0 +1,28 @@
+--================================================================================================--
+--=====  FUNCTIONS  ==============================================================================--
+--================================================================================================--
+
+---Contains functions involving saving and loading data to and from files.
+---It uses the avatar name as file name
+data = {}
+
+---Save a value in the file.
+---@param key string
+---@param value any
+function data.save(key, value) end
+
+---Returns a LuaValue of the key, or nil if not found.
+---@param key string
+---@return any
+function data.load(key) end
+
+---Returns a LuaTable of all the saved variables.
+---@return table
+function data.loadall() end
+
+---Removes the value from the file.
+---@param key string
+function remove(key) end
+
+---Completely removes the file from existence.
+function deleteFile() end
