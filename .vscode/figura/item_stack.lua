8ced179978cf76342d54a71968dd9613fdd8d049
KitCat962 <kitcatt962@gmail.com>
2022-03-18 20:36:17 -0400

    Update item_stack.lua
    Alphabetical Order :P

--- .vscode/figura/item_stack.lua
+++ .vscode/figura/item_stack.lua
@@ -1114,11 +1114,25 @@
 ---@field stack userdata
 local ItemStack = {}
 
----Sets the NBT tag of an `ItemStack`.
----
----Note: NBT is in [SNBT format](https://minecraft.fandom.com/wiki/NBT_format#SNBT_format).
----@param snbt string
-function ItemStack.setTag(snbt) end
+---Returns if the item has an enchantment glint.
+---@return boolean
+function ItemStack.hasGlint() end
+
+---Returns the item's cooldown.
+---@return number
+function ItemStack.getCooldown() end
+
+---Returns the amount of items in the stack.
+---@return number
+function ItemStack.getCount() end
+
+---Returns the damage value of the item.
+---@return number
+function ItemStack.getDamage() end
+
+---Does nothing except cause a VM Error.
+---@deprecated
+function ItemStack.getItem() end
 
 ---Returns a list of item tags that contain this item.  
 ---Item tags are assigned by datapacks.
@@ -1130,37 +1144,23 @@ function ItemStack.getItemTags() end
 ---@return table|nil
 function ItemStack.getTag() end
 
----Returns the amount of items in the stack.
----@return number
-function ItemStack.getCount() end
-
----Returns if the item has an enchantment glint.
----@return boolean
-function ItemStack.hasGlint() end
-
----Sets the damage value of the item.
----@param x number
-function ItemStack.setDamage(x) end
-
 ---Returns the item's ID.
 ---@return string
 function ItemStack.getType() end
-
----Returns the item's cooldown.
----@return number
-function ItemStack.getCooldown() end
-
----Does nothing except cause a VM Error.
----@deprecated
-function ItemStack.getItem() end
 
 ---Sets the amount of items in the stack.
 ---@param x number
 function ItemStack.setCount(x) end
 
----Returns the damage value of the item.
----@return number
-function ItemStack.getDamage() end
+---Sets the damage value of the item.
+---@param x number
+function ItemStack.setDamage(x) end
+
+---Sets the NBT tag of an `ItemStack`.
+---
+---Note: NBT is in [SNBT format](https://minecraft.fandom.com/wiki/NBT_format#SNBT_format).
+---@param snbt string
+function ItemStack.setTag(snbt) end
 
 
 --================================================================================================--
