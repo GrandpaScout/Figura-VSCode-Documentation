aab4546533a4121a35197e25f9b03822bc19b01b
KitCat962 <kitcatt962@gmail.com>
2022-03-18 21:04:28 -0400

    Update data.lua
    Alphabetical Order :P

--- .vscode/figura/data.lua
+++ .vscode/figura/data.lua
@@ -6,10 +6,8 @@
 ---It uses the avatar name as file name
 data = {}
 
----Save a value in the file.
----@param key string
----@param value any
-function data.save(key, value) end
+---Completely removes the file from existence.
+function data.deleteFile() end
 
 ---Returns a LuaValue of the key, or nil if not found.
 ---@param key string
@@ -24,5 +22,7 @@ function data.loadall() end
 ---@param key string
 function data.remove(key) end
 
----Completely removes the file from existence.
-function data.deleteFile() end
+---Save a value in the file.
+---@param key string
+---@param value any
+function data.save(key, value) end
